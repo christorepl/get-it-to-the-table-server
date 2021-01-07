b646265a6cb30258e9862d2ed1ee439779440e67
Christopher <chrisobrien416@gmail.com>
2021-01-07 12:19:16 -0500

    wiring up bga-auth

--- src/app.js
+++ src/app.js
@@ -4,17 +4,18 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const app = express()
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(express.json())
-app.use(cors({
-    origin: 'https://get-it-to-the-table.vercel.app/'
-}))
 
+app.use(cors({
+    origin: CLIENT_ORIGIN,
+  }))
+  
 app.get('/', (req, res) => {
     res.send('Hello, world! Welcome to the Get it to the Table API!')
 })
@@ -31,7 +32,7 @@ app.use('/bga-auth', require('./routes/bga-auth'))
 
 
 app.use((error, req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', 'https://get-it-to-the-table.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', CLIENT_ORIGIN);
     let response
     if (process.env.NODE_ENV === 'production') {
       response = { error: { message: 'Server Error' }}
@@ -40,7 +41,7 @@ app.use((error, req, res, next) =>{
     }
     res.status(500).json(response)
 })
-
+  
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
