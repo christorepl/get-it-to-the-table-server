6fef7ca2fda8508433e666ee3c7363709da64a74
Christopher <chrisobrien416@gmail.com>
2021-02-07 12:11:41 -0500

    major update. fixing tables.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLIENT_ORIGIN: 'https://get-it-to-the-table.vercel.app',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
     DATABASE_URL: process.env.DATABASE_URL,
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
